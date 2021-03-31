2b7f64878a03157b0d5e9fc08001b1f0a625f6a4
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-31 23:21:57 +0600

    Create data for database

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const uri = "mongodb+srv://MinhajSadik:MongoDB@cluster0.djohz.mongodb.net/organi
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
