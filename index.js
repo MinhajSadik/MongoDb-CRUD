bbd37b575de3c650caf9cf2799ca903ef5fb45c4
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-31 20:56:07 +0600

    add ES6 Tamplate string

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(`${__dirname}/index.html`);
 })
 
 client.connect(err => {
