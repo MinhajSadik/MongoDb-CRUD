255e5493d9b353616ffee7f8a3ef4743c1fc4b74
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-04-04 18:02:36 +0600

    something add

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 })
 
